4e8bfd5a99ad14aeb69eda7daf544ab02dd4cb5d
Kisshore Varma <kisshorevarma3@gmail.com>
2025-12-30 05:40:50 +0800

    modified:   video_selector.py

---
+++ video_selector.py
@@ -0,0 +1,9 @@
+def select_video(label, enable_button_callback):
+    file_path = filedialog.askopenfilename(filetypes=VIDEO_TYPES)
+    if file_path:
+        label.config(text=os.path.basename(file_path), fg="black")
+        enable_button_callback()
+        return file_path
+    else:
+        label.config(text="No file selected", fg="red")
+    return None
