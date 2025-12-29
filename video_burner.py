730ca79769728300add8bd734aab659dd2af341e
Kisshore Varma <kisshorevarma3@gmail.com>
2025-12-30 05:41:16 +0800

    modified:   video_burner.py

--- video_burner.py
+++ video_burner.py
@@ -2,12 +2,17 @@ from tkinter import filedialog
 from config import VIDEO_TYPES
 import os
 
-def select_video(label, enable_button_callback):
-    file_path = filedialog.askopenfilename(filetypes=VIDEO_TYPES)
-    if file_path:
-        label.config(text=os.path.basename(file_path), fg="black")
-        enable_button_callback()
-        return file_path
-    else:
-        label.config(text="No file selected", fg="red")
-    return None
+import subprocess
+import os
+
+def burn_subtitles(video_path, srt_path):
+    output_video = os.path.splitext(video_path)[0] + "_with_subs.mp4"
+
+    subprocess.run([
+        "ffmpeg", "-y",
+        "-i", video_path,
+        "-vf", f"subtitles={srt_path}",
+        output_video
+    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    return output_video
